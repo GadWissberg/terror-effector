70e6a872419d336ebf98c75bef7697160f06575c
Gad Wissberg <gadw17@gmail.com>
2024-03-15 17:10:26 +0200

    Editor: Working on obstacles.

--- common/core/src/main/java/com/gadarts/te/common/assets/model/Models.java
+++ common/core/src/main/java/com/gadarts/te/common/assets/model/Models.java
@@ -9,23 +9,28 @@ import static com.gadarts.te.common.assets.GameAssetsManager.PATH_SEPARATOR;
 
 @Getter
 public enum Models implements ModelDefinition {
-    WALL_WITH_RAILING(new ModelOffset(0.5F, 0F, 0.5F)),
+    WALL_WITH_RAILING(),
     WALL_WITH_RAILING_HALF(new ModelOffset(0.5F, 0F, 0F)),
-    CRATE_0_CLEAN("crate_0", "crate_0_texture_0"),
-    CRATE_0_UCI("crate_0", "crate_0_texture_1"),
-    CRATE_1_CLEAN(new ModelOffset(0.5F, 0.25F, 0.5F), "crate_1", "crate_1_texture_0"),
-    CRATE_1_UCI(new ModelOffset(0.5F, 0.25F, 0.5F), "crate_1", "crate_1_texture_1");
+    CRATE_0_CLEAN(new ModelOffset(0F, 0.5F, 0F), "crate_0", "crate_0_texture_0"),
+    CRATE_0_UCI(new ModelOffset(0F, 0.5F, 0F), "crate_0", "crate_0_texture_1"),
+    CRATE_1_CLEAN(new ModelOffset(0F, 0.25F, 0F), "crate_1", "crate_1_texture_0"),
+    CRATE_1_UCI(new ModelOffset(0F, 0.25F, 0F), "crate_1", "crate_1_texture_1"),
+    RAILING_0(new ModelOffset(0.5F, 0F, 0F));
 
     private final String filePath;
     private final ModelOffset modelOffset;
     private final String textureFileName;
+
+    Models( ) {
+        this(null, null);
+    }
 
     Models(ModelOffset offset) {
         this(offset, null, null);
     }
 
     Models(String modelFileName, String textureFileName) {
-        this(new ModelOffset(0.5F, 0.5F, 0.5F), modelFileName, textureFileName);
+        this(new ModelOffset(0F, 0F, 0F), modelFileName, textureFileName);
     }
 
     Models(ModelOffset offset, String modelFileName, String textureFileName) {
--- common/core/src/main/java/com/gadarts/te/common/definitions/Obstacles.java
+++ common/core/src/main/java/com/gadarts/te/common/definitions/Obstacles.java
@@ -10,7 +10,8 @@ public enum Obstacles implements EnvObjectDefinition {
     CRATE_0("Big Crate", Models.CRATE_0_CLEAN),
     CRATE_0_UCI("Big Crate with UCI Logo", Models.CRATE_0_UCI),
     CRATE_1("Small Crate", Models.CRATE_1_CLEAN),
-    CRATE_1_UCI("Small Crate with UCI Logo", Models.CRATE_1_UCI);
+    CRATE_1_UCI("Small Crate with UCI Logo", Models.CRATE_1_UCI),
+    RAILING_0("Railing", Models.RAILING_0);
 
     private final String displayName;
     private final Models modelDefinition;
